97c85984683b51f4a65be0bca6192886e20ebf20
ravi1016 <76171457+ravi1016@users.noreply.github.com>
2023-02-13 12:10:42 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//------Using pure JS------
+
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World";
+// document.getElementById("root").appendChild(h1);
+
+//------Using React-------
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
